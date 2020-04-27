85447da5213db46a75c4807996f97669ea156831
Micheál Keane <ffaristocrat@gmail.com>
2020-04-27 17:22:24 -0400

    black

--- chan/utils.py
+++ chan/utils.py
@@ -8,39 +8,39 @@ from sklearn.cluster import MiniBatchKMeans
 from gensim.parsing.preprocessing import strip_punctuation
 from gensim.utils import deaccent
 
-re_reply_to = re.compile(r'>>([0-9]+)(\n|$)')
-re_quote_line = re.compile(r'>.+?(\n|$)')
-re_echoes = re.compile(r'\(\(\(|\)\)\)')
-re_parentheses = re.compile(r'(|\)\()')
+re_reply_to = re.compile(r">>([0-9]+)(\n|$)")
+re_quote_line = re.compile(r">.+?(\n|$)")
+re_echoes = re.compile(r"\(\(\(|\)\)\)")
+re_parentheses = re.compile(r"(|\)\()")
 re_punc_to_space = re.compile(r'[\n\r,/:"“”\]\[}{()!\t*&^@~-]')
-re_punc_to_none = re.compile(r'[;<>]')
-re_question = re.compile(r'\?')
-re_period = re.compile(r'\.')
-re_pol_board = re.compile(r'/pol/')
-re_b_board = re.compile(r'/b/')
-re_chan_board = re.compile(r'/.+/?')
+re_punc_to_none = re.compile(r"[;<>]")
+re_question = re.compile(r"\?")
+re_period = re.compile(r"\.")
+re_pol_board = re.compile(r"/pol/")
+re_b_board = re.compile(r"/b/")
+re_chan_board = re.compile(r"/.+/?")
 re_youtube_link = re.compile(r"http(s|)://.youtube\.com[^\s]+[\s]?")
 re_link = re.compile(r"http(s|)://[^\s]+[\s]?")
-re_numbers = re.compile(r'([0-9]+)')
-re_ellipsis = re.compile(r'\.\.\.')
+re_numbers = re.compile(r"([0-9]+)")
+re_ellipsis = re.compile(r"\.\.\.")
 
 
 def clean_string(string):
     # Empty strings
-    if not string or string == 'N':
+    if not string or string == "N":
         return None
 
     string = deaccent(string).lower()
 
     # Remove quote text
-    string = re.sub(re_reply_to, '', string)
-    string = re.sub(re_quote_line, '', string)
+    string = re.sub(re_reply_to, "", string)
+    string = re.sub(re_quote_line, "", string)
 
-    string = re.sub(re_youtube_link, ' YOUTUBELINK ', string)
-    string = re.sub(re_link, ' WEBLINK ', string)
-    string = re.sub(re_pol_board, ' pol ', string)
-    string = re.sub(re_b_board, ' RANDOMBOARD ', string)
-    string = re.sub(re_chan_board, ' CHANBOARD ', string)
+    string = re.sub(re_youtube_link, " YOUTUBELINK ", string)
+    string = re.sub(re_link, " WEBLINK ", string)
+    string = re.sub(re_pol_board, " pol ", string)
+    string = re.sub(re_b_board, " RANDOMBOARD ", string)
+    string = re.sub(re_chan_board, " CHANBOARD ", string)
 
     string = strip_punctuation(string)
 
@@ -58,7 +58,7 @@ def clean_string(string):
     # Replace all other punc to spaces and remove whitespace in between
     # string = re.sub(re_punc_to_space, ' ', string)
 
-    string = ' '.join([word for word in [w.strip() for w in string.split()]])
+    string = " ".join([word for word in [w.strip() for w in string.split()]])
 
     return string if string else None
 
@@ -66,9 +66,9 @@ def clean_string(string):
 class Benchmark(object):
     def __init__(self, msg: str):
         self.msg = msg
-    
+
     def __enter__(self):
-        print(f'Starting {self.msg}')
+        print(f"Starting {self.msg}")
         self.start = timeit.default_timer()
         return self
 
@@ -76,7 +76,7 @@ class Benchmark(object):
         t = timeit.default_timer() - self.start
         print(make_msg(self.msg, t))
         self.time = t
- 
+
     def current(self):
         t = timeit.default_timer() - self.start
         print(make_msg(self.msg, t))
@@ -88,15 +88,15 @@ def make_msg(msg, t):
     h, m = divmod(m, 60)
     d, h = divmod(h, 24)
     if d:
-        msg = f'{msg}: {d:.0f} days, ' \
-              f'{h:.0f} hours, {m:.0f} minutes, {s:.0f} seconds'
+        msg = (
+            f"{msg}: {d:.0f} days, " f"{h:.0f} hours, {m:.0f} minutes, {s:.0f} seconds"
+        )
     elif h:
-        msg = f'{msg}: ' \
-              f'{h:.0f} hours, {m:.0f} minutes, {s:.0f} seconds'
+        msg = f"{msg}: " f"{h:.0f} hours, {m:.0f} minutes, {s:.0f} seconds"
     elif m:
-        msg = f'{msg}: {m:.0f} minutes, {s:.0f} seconds'
+        msg = f"{msg}: {m:.0f} minutes, {s:.0f} seconds"
     else:
-        msg = f'{msg}: {s:.3f} seconds'
+        msg = f"{msg}: {s:.3f} seconds"
 
     return msg
 
@@ -105,7 +105,7 @@ def benchmark(method):
     @wraps(method)
     def f(*args, **kwargs):
         start = timeit.default_timer()
-        print(f'Starting {method.__name__}')
+        print(f"Starting {method.__name__}")
         result = method(*args, **kwargs)
         t = timeit.default_timer() - start
         print(make_msg(method.__name__, t))
@@ -114,10 +114,8 @@ def benchmark(method):
     return f
 
 
-def cluster(df, n_clusters: int=8):
+def cluster(df, n_clusters: int = 8):
     matrix = df.astype(np.float64).values
-    df['cluster'] = MiniBatchKMeans(n_clusters=n_clusters).fit_predict(matrix)
-    
-    return df[['cluster']]
+    df["cluster"] = MiniBatchKMeans(n_clusters=n_clusters).fit_predict(matrix)
 
-
+    return df[["cluster"]]
--- opeds/scrape_articles.py
+++ opeds/scrape_articles.py
@@ -5,29 +5,30 @@ import pandas as pd
 
 from selenium import webdriver
 
-chromedriver = '/usr/local/Caskroom/chromedriver'
+chromedriver = "/usr/local/Caskroom/chromedriver"
 driver = webdriver.Chrome(executable_path=chromedriver)
 
-domain = 'https://infoweb-newsbank-com.dclibrary.idm.oclc.org'
+domain = "https://infoweb-newsbank-com.dclibrary.idm.oclc.org"
 
 
 def login(user_id: int):
-    base_url = f'{domain}/resources/?p=WORLDNEWS'
+    base_url = f"{domain}/resources/?p=WORLDNEWS"
     driver.get(base_url)
-    field = driver.find_element_by_css_selector(
-        'input[type="text"][name="user"]')
+    field = driver.find_element_by_css_selector('input[type="text"][name="user"]')
     field.send_keys(user_id)
     field.submit()
 
 
 def run_search(name):
-    first, _, last = name.partition(' ')
+    first, _, last = name.partition(" ")
 
-    url = f'{domain}/resources/?p=WORLDNEWS&b=results&action=search' \
-          f'&t=product%3AAWNB%21Access%2BWorld%2BNews/country%3AUSA%21USA/' \
-          f'stp%3ANewspaper%21Newspaper&fld0=Author' \
-          f'&val0=%22{first}%20{last}%22%20or%20%22{last}%2C%20{first}%22' \
-          f'&bln1=AND&fld1=YMD_date&val1=&sort=YMD_date%3AA'
+    url = (
+        f"{domain}/resources/?p=WORLDNEWS&b=results&action=search"
+        f"&t=product%3AAWNB%21Access%2BWorld%2BNews/country%3AUSA%21USA/"
+        f"stp%3ANewspaper%21Newspaper&fld0=Author"
+        f"&val0=%22{first}%20{last}%22%20or%20%22{last}%2C%20{first}%22"
+        f"&bln1=AND&fld1=YMD_date&val1=&sort=YMD_date%3AA"
+    )
     driver.get(url)
 
 
@@ -35,40 +36,40 @@ def get_article_urls(name) -> pd.DataFrame:
     links = []
 
     while True:
-        elements = driver.find_elements_by_css_selector('a.nb-doc-link')
-        links.extend([e.get_attribute('href') for e in elements])
+        elements = driver.find_elements_by_css_selector("a.nb-doc-link")
+        links.extend([e.get_attribute("href") for e in elements])
 
         try:
-            next_page = driver.find_element_by_css_selector('li.pager-next')
+            next_page = driver.find_element_by_css_selector("li.pager-next")
         except BaseException:
             break
 
         next_page.click()
 
-    df = pd.DataFrame(links, columns=['urls'])
+    df = pd.DataFrame(links, columns=["urls"])
 
-    df['name'] = name
+    df["name"] = name
 
     return df
 
 
 def parse_article(row) -> Tuple:
-    driver.get(row['urls'])
-    body = driver.find_element_by_css_selector('div.body').text
-    byline = driver.find_element_by_css_selector('span.val').text
+    driver.get(row["urls"])
+    body = driver.find_element_by_css_selector("div.body").text
+    byline = driver.find_element_by_css_selector("span.val").text
 
-    return row['urls'], row['name'], body, byline
+    return row["urls"], row["name"], body, byline
 
 
 def read_names(filename) -> List[str]:
-    names = [n for n in open(filename).read().split('\n') if n]
+    names = [n for n in open(filename).read().split("\n") if n]
     print(names)
     return names
 
 
 def main():
-    user_id = os.environ.get('USER_ID')
-    filename = 'senior_admin_officials.txt'
+    user_id = os.environ.get("USER_ID")
+    filename = "senior_admin_officials.txt"
 
     login(user_id)
 
@@ -80,14 +81,14 @@ def main():
         run_search(name)
         df = df.append(get_article_urls(name))
 
-    df.to_csv('urls.txt', index=False)
-    df = pd.read_csv('urls.txt', header=0)
+    df.to_csv("urls.txt", index=False)
+    df = pd.read_csv("urls.txt", header=0)
 
-    df2 = df.apply(lambda x: parse_article(x), axis=1, result_type='expand')
-    df2.to_csv('articles.txt', headers=True, index=False)
+    df2 = df.apply(lambda x: parse_article(x), axis=1, result_type="expand")
+    df2.to_csv("articles.txt", headers=True, index=False)
 
     driver.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
